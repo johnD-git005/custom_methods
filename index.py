aaa823fa1c482335225d47412390f50601d55c9c
johnD-git005 <johndakshak@gmail.com>
2025-09-15 13:04:07 +0100

    Initial commit

--- index.py
+++ index.py
@@ -2,3 +2,15 @@ from my_module import Array
 
 fruits = Array()
 
+fruits.append("mango")
+fruits.append("banana")
+fruits.append(5)
+print(fruits)
+fruits.append(3)
+fruits.append(9)
+print(fruits)
+fruits.insert(2, "apple")
+fruits.pop()
+fruits.pop(0)
+fruits.remove("apple")
+print(fruits)
